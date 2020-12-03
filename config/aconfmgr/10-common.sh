3df3b56c0af455264fff974ade69928976a493fa
Karl Meakin <karlwfmeakin@gmail.com>
2020-12-03 17:06:35 +0000

    fmt

--- config/aconfmgr/10-common.sh
+++ config/aconfmgr/10-common.sh
@@ -1,23 +1,25 @@
+#!/usr/bin/env bash
+
 # locale
-echo "LANG=en_GB.UTF-8" > "$(CreateFile /etc/locale.conf)"
-cat >> "$(GetPackageOriginalFile glibc /etc/locale.gen)" <<EOF
+echo "LANG=en_GB.UTF-8" >"$(CreateFile /etc/locale.conf)"
+cat >>"$(GetPackageOriginalFile glibc /etc/locale.gen)" <<EOF
 en_US.UTF-8 UTF-8
 en_GB.UTF-8 UTF-8
 EOF
 
 # keymap
-echo "KEYMAP=uk" > "$(CreateFile /etc/vconsole.conf)"
+echo "KEYMAP=uk" >"$(CreateFile /etc/vconsole.conf)"
 
 # timezone
 CreateLink /etc/localtime /usr/share/zoneinfo/Europe/London
 
 # sudoers
-cat >> "$(GetPackageOriginalFile sudo /etc/sudoers)" <<EOF
+cat >>"$(GetPackageOriginalFile sudo /etc/sudoers)" <<EOF
 %wheel ALL=(ALL) ALL
 EOF
 
 # sddm
-cat >> "$(CreateFile /etc/sddm.conf.d/kde_settings.conf)" <<EOF
+cat >>"$(CreateFile /etc/sddm.conf.d/kde_settings.conf)" <<EOF
 [Theme]
 Current=breeze
 
--- config/aconfmgr/20-desktop.sh
+++ config/aconfmgr/20-desktop.sh
@@ -1,14 +1,16 @@
-if [[ "${HOSTNAME}" = "karl-desktop" ]]; then
+#!/usr/bin/env bash
+
+if [[ "${HOSTNAME}" != "karl-desktop" ]]; then return; fi
 
 # hostname
-echo "karl-desktop" > "$(CreateFile /etc/hostname)"
-cat >> "$(GetPackageOriginalFile filesystem /etc/hosts)" <<EOF
+echo "karl-desktop" >"$(CreateFile /etc/hostname)"
+cat >>"$(GetPackageOriginalFile filesystem /etc/hosts)" <<EOF
 127.0.0.1	localhost
 ::1	localhost
 EOF
 
 # fstab
-cat >> "$(GetPackageOriginalFile filesystem /etc/fstab)" <<EOF
+cat >>"$(GetPackageOriginalFile filesystem /etc/fstab)" <<EOF
 # /dev/sdc2 UUID=5714353f-adaa-4bb4-a2ec-451549d0a3b7
 LABEL=ROOT          	/         	ext4      	rw,relatime	0 1
 
@@ -16,4 +18,3 @@ LABEL=ROOT          	/         	ext4      	rw,relatime	0 1
 LABEL=BOOT          	/boot     	vfat      	rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro	0 2
 EOF
 
-fi
