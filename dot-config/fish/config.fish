3a3b2c0c7685e6ebaa9588067132246e36bcb43d
Karl Meakin <karl.meakin@arm.com>
2025-05-23 16:39:54 +0100

    replace some uses of `add-to-list` and `set` with `export`

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -12,13 +12,14 @@ export XDG_STATE_HOME="$HOME/.local/state"
 alias add-to-list="set --prepend --global --export"
 
 # Homebrew
-add-to-list HOMEBREW_PREFIX /opt/homebrew
-add-to-list HOMEBREW_CELLAR /opt/homebrew/Cellar
-add-to-list HOMEBREW_REPOSITORY /opt/homebrew
+export HOMEBREW_PREFIX="/opt/homebrew"
+export HOMEBREW_REPOSITORY="/opt/homebrew"
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
 
 add-to-list PATH /opt/homebrew/bin
 add-to-list PATH /opt/homebrew/sbin
 add-to-list INFOPATH /opt/homebrew/share/info
+add-to-list MANPATH /opt/homebrew/share/man
 
 ## LLVM
 add-to-list PATH "/opt/homebrew/opt/llvm/bin"
@@ -26,7 +27,7 @@ add-to-list LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
 add-to-list CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 
 ## GCC
-add-to-list PATH /opt/homebrew/opt/binutils/bin
+add-to-list PATH "/opt/homebrew/opt/binutils/bin"
 add-to-list LDFLAGS "-L/opt/homebrew/opt/binutils/lib"
 add-to-list CPPFLAGS "-I/opt/homebrew/opt/binutils/include"
 
@@ -40,8 +41,8 @@ alias vi="nvim"
 alias vim="nvim"
 alias nvimdiff="nvim -d"
 alias vimdiff="nvimdiff"
-set --global --export EDITOR nvim
-set --global --export VISUAL nvim
+export EDITOR=nvim
+export VISUAL=nvim
 
 # Convience aliases
 alias which="type --all"
