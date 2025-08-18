3efad5de109de60f322b99c94eeedc6015a6a440
Karl Meakin <karl.meakin@arm.com>
2025-08-18 21:45:19 +0100

    fish: use `$()` consistently

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -130,10 +130,10 @@ abbr -a !! --position anywhere --function last-history-item
 # Theme
 fish_config theme choose 'Solarized Dark'
 
-if test -d (brew --prefix)"/share/fish/completions"
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
+if test -d $(brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path $(brew --prefix)/share/fish/completions
 end
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+if test -d $(brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path $(brew --prefix)/share/fish/vendor_completions.d
 end
